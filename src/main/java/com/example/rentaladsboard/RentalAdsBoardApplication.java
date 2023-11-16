d6e8842217b5f0e7be8cf3acc6d280a4f10a725c
Phoenix0356 <1242780667@qq.com>
2023-11-16 19:36:37 +0800

    20231116

---
+++ src/main/java/com/example/rentaladsboard/RentalAdsBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentaladsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalAdsBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalAdsBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentaladsboard/RentalAdsBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentaladsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalAdsBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
